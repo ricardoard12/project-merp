5c9d47be63bb55e2263097ddd7321b266020f543
simon.hub3r@gmail.com <simon.hub3r@gmail.com@04e48588-5cae-8dbb-50e2-7f047fe7dc0a>
2011-10-29 14:27:03 +0000

    Added Host!

--- DAL/MerpDataManager.cs
+++ DAL/MerpDataManager.cs
@@ -14,43 +14,43 @@ namespace DAL
 {
     public class MerpDataManager
     {
-        DL.db_MERPEntities1 modell = new db_MERPEntities1();
+        readonly DL.db_MERPEntities1 _modell = new db_MERPEntities1();
 
         public List<DL.tbl_Prd> GetProducts(int PrdCat, int Anzahl, int Start)
         {
-            return (from P in modell.tbl_Prd where P.PrdPrdcat_ == PrdCat orderby P.Prd_ select P).Skip(Start).Take(Anzahl).ToList();
+            return (from P in _modell.tbl_Prd where P.PrdPrdcat_ == PrdCat orderby P.Prd_ select P).Skip(Start).Take(Anzahl).ToList();
         }
 
         public Int32 GetProductCategorieCount(int PrdCat)
         {
-            return (from P in modell.tbl_Prd where P.PrdPrdcat_ == PrdCat orderby P.Prd_ select P).Count();
+            return (from P in _modell.tbl_Prd where P.PrdPrdcat_ == PrdCat orderby P.Prd_ select P).Count();
         }
 
         public List<DL.tbl_Prdcat> GetAllProductCategories() {
-            return (from P in modell.tbl_Prdcat orderby P.Prdcat_ select P).ToList();
+            return (from P in _modell.tbl_Prdcat orderby P.Prdcat_ select P).ToList();
         }
 
         public Int32 GetProductCategorieCount() {
-            return (from P in modell.tbl_Prdcat select P).Count();
+            return (from P in _modell.tbl_Prdcat select P).Count();
         }
 
 
         public List<DL.tbl_Usr> GetUserByIdent(string usrident, int Anzahl, int Start) {
-            return (from u in modell.tbl_Usr where u.usrIdent == usrident select u).Skip(Start).Take(Anzahl).ToList();
+            return (from u in _modell.tbl_Usr where u.usrIdent == usrident select u).Skip(Start).Take(Anzahl).ToList();
         }
 
         public Int32 GetUserCountByIdent(string usrident) {
-            return (from u in modell.tbl_Usr where u.usrIdent == usrident select u).Count();
+            return (from u in _modell.tbl_Usr where u.usrIdent == usrident select u).Count();
         }
 
         public void UpdateUser(User usr) {
-            tbl_Usr User = (from u in modell.tbl_Usr where u.usr_ == usr.UsrId select u).Single();
+            tbl_Usr User = (from u in _modell.tbl_Usr where u.usr_ == usr.UsrId select u).Single();
             User.usr_ = usr.UsrId;
             User.usrName = usr.UsrName;
             User.usrLogedin = usr.UsrLogin;
             User.usrPassword = usr.UsrPassword;
             User.usrIdent = usr.UsrIdent;
-            modell.SaveChanges();
+            _modell.SaveChanges();
         }
 
         public void AddUser(User usr) {
@@ -61,9 +61,9 @@ namespace DAL
             User.usrPassword = usr.UsrPassword;
             User.usrIdent = usr.UsrIdent;
 
-            modell.tbl_Usr.AddObject(User);
+            _modell.tbl_Usr.AddObject(User);
             try {
-                modell.SaveChanges();
+                _modell.SaveChanges();
             } catch(Exception e) {
                 
             }
