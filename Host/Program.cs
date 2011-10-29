be48dfeccff8bb17dd73d8574ad069d505bcf13a
simon.hub3r <simon.hub3r@gmail.com>
2011-10-29 14:27:03 +0000

    Added Host!

---
+++ Host/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using BL;
+using BL.Service;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost sh = new ServiceHost(typeof(MERPService));
+            Console.Write("Host wird gestartet...\n");   
+            sh.Open();
+            Console.Write("Host wurde gestartet\n");
+            Console.Write("Drücken Sie eine Taste um zu beenden\n");
+            Console.Read();
+            sh.Close();
+                       
+        }
+    }
+}
