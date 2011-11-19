f725caac645b038615d63e9463797e5141611c22
simon.hub3r <simon.hub3r@gmail.com>
2011-11-19 14:58:38 +0000

    Added 1. TreeView

---
+++ WpfApplication1/ViewModel/NavCommands/TreeViewCommandCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using FrontEnd.Properties;
+using FrontEnd.Model.Stammdaten;
+using FrontEnd.ViewModel.Stammdaten.Customer;
+using FrontEnd.ViewModel.Stammdaten.Product;
+
+namespace FrontEnd.ViewModel.NavCommands {
+    public class TreeViewCommandCategory : MainWindowViewModel
+    {
+        public string Name { get; set; }
+        public List<CommandViewModel> Commands { get; set; }
+
+        public TreeViewCommandCategory(string name) {
+            Name = name;
+        }
+    }
+}
