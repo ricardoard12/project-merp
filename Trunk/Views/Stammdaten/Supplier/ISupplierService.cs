40563ee57eed2c0969219630bc6659ff03a83bb3
simon.hub3r@gmail.com <simon.hub3r@gmail.com@04e48588-5cae-8dbb-50e2-7f047fe7dc0a>
2012-03-22 13:50:51 +0000

    Publish Supplier for Front End

---
+++ Trunk/Views/Stammdaten/Supplier/ISupplierService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Views.Stammdaten.Supplier
+{
+    public interface ISupplierService
+    {
+        void DeleteSupplier(ISupplierView supplier);
+
+
+        void AddSupplier(ISupplierView supplier);
+
+
+        PagedResult<ISupplierView> AllSupplier();
+
+
+        ISupplierView SupplierByPrimaryKey(int primaryKey);
+    }
+}
+
