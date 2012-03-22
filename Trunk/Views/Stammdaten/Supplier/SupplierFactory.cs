76fb85bfc3e56304bb8bedf5e91f3eec2d30fcda
simon.hub3r@gmail.com <simon.hub3r@gmail.com@04e48588-5cae-8dbb-50e2-7f047fe7dc0a>
2012-03-22 12:46:40 +0000

    Add SupplierView

---
+++ Trunk/Views/Stammdaten/Supplier/SupplierFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace Views.Stammdaten.Supplier
+{
+    public class SupplierFactory
+    {
+
+        public static ISupplierView createNew()
+        {
+            return new SupplierView();
+        }
+
+        public static ISupplierView createNew(int supId, int? supNumber, string supFirstname, string supLastname, string supContactname, int? supUsrId, bool? supIsCompany)
+        {
+            return new SupplierView(supId, supNumber, supFirstname, supLastname, supContactname, supUsrId, supIsCompany);
+        }
+    }
+}
