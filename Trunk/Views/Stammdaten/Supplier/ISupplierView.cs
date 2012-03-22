1e66e1ab7a46ba3a8110f8d76ffd4a64f3b4108b
simon.hub3r <simon.hub3r@gmail.com>
2012-03-22 12:46:40 +0000

    Add SupplierView

---
+++ Trunk/Views/Stammdaten/Supplier/ISupplierView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Views.Stammdaten.Supplier
+{
+    public interface ISupplierView
+    {
+        int SupId { get; set; }
+        int? SupNumber { get; set; }
+        string SupFirstname { get; set; }
+        string SupLastname { get; set; }
+        string SupContactname { get; set; }
+        int?  SupUsrId{ get; set; }
+        bool? SupIsCompany { get; set; }
+    }
+}
---
+++ Trunk/Views/Stammdaten/Supplier/SupplierView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Views.Stammdaten.Supplier
+{
+    class SupplierView : ISupplierView
+    {
+        public int SupId { get; set; }
+        public int? SupNumber { get; set; }
+        public string SupFirstname { get; set; }
+        public string SupLastname { get; set; }
+        public string SupContactname { get; set; }
+        public int? SupUsrId { get; set; }
+        public bool? SupIsCompany { get; set; }
+
+        
+        public SupplierView() {
+
+        }
+
+
+        public SupplierView(int supId, int? supNumber, string supFirstname, string supLastname, string supContactname, int? supUsrId, bool? supIsCompany)
+        {
+            this.SupId = supId;
+            this.SupNumber = supNumber;
+            this.SupFirstname = supFirstname;
+            this.SupLastname = supLastname;
+            this.SupContactname = supContactname;
+            this.SupUsrId = supUsrId;
+            this.SupIsCompany = supIsCompany;
+        }
+    }
+}
