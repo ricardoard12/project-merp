bd496beceea45683739c4b2eb843fbd9b9cbd158
simon.hub3r <simon.hub3r@gmail.com>
2012-03-22 12:18:43 +0000

    Fixed Database Path, Add Customer things to Access Customer on DB

---
+++ Trunk/BL/Service/Stammdaten/Customer/CustomerService.cs
@@ -0,0 +1,12 @@
+﻿using BL.Service.Stammdaten.Customer;
+using Views.Stammdaten;
+
+//HBS
+
+namespace BL.Service
+{
+    public partial class RootService: ICustomerService
+    {
+        public ICustomerView 
+    }
+}
