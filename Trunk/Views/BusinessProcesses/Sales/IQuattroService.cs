577625a9d9e94272dfbdb7d20cee9b21097b9337
elia.kaufmann94 <elia.kaufmann94@gmail.com>
2012-03-23 13:30:40 +0000

    Added logic with classes and stuff for "Order" process

---
+++ Trunk/Views/BusinessProcesses/Sales/IQuattroService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using Views.BusinessProcesses.Sales.Order;
+
+namespace Views.BusinessProcesses.Sales
+{
+    [ServiceContract]
+    [ServiceKnownType(typeof(OrderView))]
+    public class IQuattroService
+    {
+        [OperationContract]
+        void DeleteSupplier(IOrderView order);
+
+        [OperationContract]
+        void AddSupplier(IOrderView order);
+
+        [OperationContract]
+        PagedResult<IOrderView> AllOrder();
+
+        [OperationContract]
+        IOrderView OrderByPrimaryKey(int primaryKey);
+
+        [OperationContract]
+        void UpdateOrder(IOrderView order);
+    }
+}
---
+++ Trunk/Views/BusinessProcesses/Sales/Order/OrderFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Views.BusinessProcesses.Sales.Order;
+
+namespace Views.BusinessProcesses.Sales.Order
+{
+    public class OrderFactory
+    {
+        public static IOrderView createNew()
+        {
+            return new OrderView();
+        }
+
+        public static IOrderView createNew(int offerId, int offerNumber, int offerCustomer, DateTime offerCreateDate)
+        {
+            return new OrderView(offerId, offerNumber, offerCustomer, offerCreateDate);
+        }
+    }
+}
