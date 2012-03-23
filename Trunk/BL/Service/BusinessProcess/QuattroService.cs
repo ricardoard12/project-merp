65e1ea5b4ec07b33c6cad81bf288b417dc0b6e78
simon.hub3r@gmail.com <simon.hub3r@gmail.com@04e48588-5cae-8dbb-50e2-7f047fe7dc0a>
2012-03-23 14:06:15 +0000

    Add QuattroService

---
+++ Trunk/BL/Service/BusinessProcess/QuattroService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Views.BusinessProcesses.Sales;
+using Views.BusinessProcesses.Sales.Order;
+
+
+namespace BL.Service
+{
+    public partial class RootService : IQuattroService 
+    {
+        public void AddQuattro(IOrderView view)
+        {
+            
+        }
+
+        public IOrderView QuattroByPrimaryKey(int primaryKey)
+        {
+            
+        }
+
+        public IList<IOrderView> AllQuattros()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<IOrderView> BySpecifiedType()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
