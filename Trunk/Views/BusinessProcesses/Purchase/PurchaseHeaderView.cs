132a38e0574706e11a322686084b32723b83b57c
simon.hub3r@gmail.com <simon.hub3r@gmail.com@04e48588-5cae-8dbb-50e2-7f047fe7dc0a>
2012-03-29 08:00:18 +0000

    Add PurchaseThings

---
+++ Trunk/Views/BusinessProcesses/Purchase/PurchaseHeaderView.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Views.BusinessProcesses.Purchase
+{
+    internal class PurchaseHeaderView : IPurchaseHeaderView
+    {
+
+        public PurchaseHeaderView()
+        {
+        }
+
+        public PurchaseHeaderView(int purchaseId, int? purchaseNumber, int? purchaseCustomer,
+                                  DateTime? purchaseCreateDate, int? purchasetype)
+        {
+            this.PurchaseHeaderId = purchaseId;
+            this.PurchaseHeaderNumber = purchaseNumber;
+            this.PurchaseHeaderCustomer = purchaseCustomer;
+            this.PurchaseHeaderCreateDate = purchaseCreateDate;
+            this.PurchaseHeaderType = purchasetype;
+        }
+
+
+        public int PurchaseHeaderId { get; set; }
+        public int? PurchaseHeaderNumber { get; set; }
+        public int? PurchaseHeaderCustomer { get; set; }
+        public DateTime? PurchaseHeaderCreateDate { get; set; }
+        public int? PurchaseHeaderType { get; set; }
+        public bool IsValid { get; private set; }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("PurchaseHeaderId", PurchaseHeaderId);
+            info.AddValue("PurchaseHeaderNumber", PurchaseHeaderNumber);
+            info.AddValue("PurchaseHeaderCustomer", PurchaseHeaderCustomer);
+            info.AddValue("PurchaseHeaderCreateDate", PurchaseHeaderCreateDate);
+            info.AddValue("PurchaseHeaderType", PurchaseHeaderType);
+        }
+
+        protected PurchaseHeaderView(SerializationInfo info, StreamingContext context)
+        {
+            PurchaseHeaderId = (int)info.GetValue("PurchaseHeaderId", typeof(int));
+            PurchaseHeaderNumber = (int?)info.GetValue("PurchaseHeaderNumber", typeof(int));
+            PurchaseHeaderCustomer = (int?)info.GetValue("PurchaseHeaderCustomer", typeof(int?));
+            PurchaseHeaderCreateDate = (DateTime?)info.GetValue("PurchaseHeaderCreateDate", typeof(DateTime?));
+            PurchaseHeaderType = (int?)info.GetValue("PurchaseHeaderType", typeof(int?));
+        }
+
+    }
+}
---
+++ Trunk/Views/BusinessProcesses/Purchase/PurchaseItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Views.BusinessProcesses.Purchase
+{
+    [Serializable]
+    internal class PurchaseItem : IPurchaseItem
+    {
+
+        public int PuiId { get; set; }
+        public int PuiNumber { get; set; }
+        public int PuiSahId { get; set; }
+        public int PuiPrdId { get; set; }
+        public float Puivat { get; set; }
+        public float PuiDiscount { get; set; }
+
+
+        public PurchaseItem()
+        {
+        }
+
+        public PurchaseItem(int id, int Number, int SahId, int PrdId, float vat, float discount)
+        {
+            this.PuiId = id;
+            this.PuiNumber = Number;
+            this.PuiSahId = SahId;
+            this.Puivat = vat;
+            this.PuiPrdId = PrdId;
+            this.PuiDiscount = discount;
+        }
+
+        protected PurchaseItem(SerializationInfo info, StreamingContext context)
+        {
+            PuiId = (int) info.GetValue("PuiId", typeof (int));
+            PuiNumber = (int) info.GetValue("PuiNumber", typeof (int));
+            PuiSahId = (int) info.GetValue("PuiSahId", typeof (int));
+            PuiPrdId = (int) info.GetValue("PuiPrdId", typeof (int));
+            Puivat = (float) info.GetValue("Puivat", typeof (float));
+            PuiDiscount = (float) info.GetValue("PuiDiscount", typeof (float));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("PuiId", PuiId);
+            info.AddValue("PuiNumber", PuiNumber);
+            info.AddValue("PuiSahId", PuiSahId);
+            info.AddValue("PuiPrdId", PuiPrdId);
+            info.AddValue("Puivat", Puivat);
+            info.AddValue("PuiDiscount", PuiDiscount);
+        }
+
+
+    }
+}
