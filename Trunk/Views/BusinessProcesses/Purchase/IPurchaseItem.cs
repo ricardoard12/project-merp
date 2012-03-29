a249cf29103ae8ea2efc7fbeac52f82cb95e6ce9
simon.hub3r <simon.hub3r@gmail.com>
2012-03-29 08:00:18 +0000

    Add PurchaseThings

---
+++ Trunk/Views/BusinessProcesses/Purchase/IPurchaseItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Views.BusinessProcesses.Purchase
+{
+    public interface IPurchaseItem : ISerializable 
+    {
+        int PuiId { get; set; }
+        int PuiNumber { get; set; }
+        int PuiSahId { get; set; }
+        int PuiPrdId { get; set; }
+        float Puivat { get; set; }
+        float PuiDiscount { get; set; }
+    }
+}
---
+++ Trunk/Views/BusinessProcesses/Purchase/PurchaseFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Views.BusinessProcesses.Purchase
+{
+    public class PurchaseFactory
+    {
+        public static IPurchaseHeaderView createNewPurchaseHeader()
+        {
+            return  new PurchaseHeaderView();
+        }
+
+        public static IPurchaseHeaderView createNewPurchaseHeader(int purchaseId, int? purchaseNumber, int? purchaseCustomer, DateTime? purchaseCreateDate, int? purchasetype)
+        {
+            return  new PurchaseHeaderView(purchaseId, purchaseNumber, purchaseCustomer, purchaseCreateDate, purchasetype);
+        }
+
+
+        public static IPurchaseItem createNewPurchaseItem()
+        {
+            return new PurchaseItem();
+        }
+
+        public  static IPurchaseItem createNewPruchaseItem(int id, int number, int SahId, int PrdId, float vat, float discount)
+        {
+            return  new PurchaseItem(id, number, SahId, PrdId, vat, discount);
+        }
+    }
+}
