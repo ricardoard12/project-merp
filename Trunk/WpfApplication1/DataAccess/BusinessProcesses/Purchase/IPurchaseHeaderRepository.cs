e4663424f107d86b9040e9597bf9fe950079001c
k3v1n.ch3n21 <k3v1n.ch3n21@gmail.com>
2012-03-29 09:46:03 +0000

    added PurchaseHeaderRepo + Interface PurchaseHeader GUI PurchaseHeaderViewModel

---
+++ Trunk/WpfApplication1/DataAccess/BusinessProcesses/Purchase/IPurchaseHeaderRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Views.BusinessProcesses.Purchase;
+
+namespace WpfApplication1.DataAccess.BusinessProcesses.Purchase
+{
+    public interface IPurchaseHeaderRepository
+    {
+        void AddPurchaseHeader(IPurchaseHeaderView purchaseHeader);
+
+        IList<IPurchaseHeaderView> GetAllPurchaseHeader();
+
+        void DeletePurchaseHeader(IPurchaseHeaderView purchaseHeader);
+
+        void UpdatePurchaseHeader(IPurchaseHeaderView purchaseHeader);
+
+        IPurchaseHeaderView GetPurchaseHeaderByPrimaryKey(int primaryKey);
+    }
+}
