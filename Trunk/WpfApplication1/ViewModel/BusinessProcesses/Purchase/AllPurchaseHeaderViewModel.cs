5064cbab18f938e4526b1321675bd4a4cfd49d2e
k3v1n.ch3n21@gmail.com <k3v1n.ch3n21@gmail.com@04e48588-5cae-8dbb-50e2-7f047fe7dc0a>
2012-03-30 06:56:15 +0000

    implemented AllPurchaseHeaderViewModel

---
+++ Trunk/WpfApplication1/ViewModel/BusinessProcesses/Purchase/AllPurchaseHeaderViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using FrontEnd.DataAccess.Stammdaten.Product;
+using FrontEnd.ViewModel;
+using FrontEnd.ViewModel.Stammdaten.Product;
+using Views.BusinessProcesses.Purchase;
+using Views.Stammdaten.Product;
+using WpfApplication1.DataAccess.BusinessProcesses.Purchase;
+using WpfApplication1.DataAccess.BusinessProcesses.Sales;
+
+namespace WpfApplication1.ViewModel.BusinessProcesses.Purchase
+{
+    public class AllPurchaseHeaderViewModel : WorkspaceViewModel, IAllPurchaseHeaderViewModel
+    {
+        private PurchaseHeaderRepository purchaseHeaderRepository;
+        private PurchaseHeaderViewModel purchaseHeaderViewModel;
+        private ObservableCollection<IPurchaseHeaderView> purchaseHeaderView;
+
+        public AllPurchaseHeaderViewModel()
+        {
+            purchaseHeaderRepository = new PurchaseHeaderRepository();
+            purchaseHeaderViewModel = new PurchaseHeaderViewModel();
+        }
+
+        public ObservableCollection<IPurchaseHeaderView> PurchaseHeaders
+        {
+            get
+            {
+                if (purchaseHeaderView == null || purchaseHeaderView.Count == 0)
+                {
+                    purchaseHeaderView = new ObservableCollection<IPurchaseHeaderView>(purchaseHeaderRepository.GetAllPurchaseHeader());
+                }
+                return purchaseHeaderView;
+            }
+        }
+    }
+}
