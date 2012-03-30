6b3817b47f22dfe3c4f087608be360b197b6580a
k3v1n.ch3n21 <k3v1n.ch3n21@gmail.com>
2012-03-30 06:56:15 +0000

    implemented AllPurchaseHeaderViewModel

---
+++ Trunk/WpfApplication1/ViewModel/BusinessProcesses/Purchase/IAllPurchaseHeaderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Views.BusinessProcesses.Purchase;
+using Views.Stammdaten.Product;
+
+namespace WpfApplication1.ViewModel.BusinessProcesses.Purchase
+{
+    public interface IAllPurchaseHeaderViewModel
+    {
+        ObservableCollection<IPurchaseHeaderView> PurchaseHeaders { get; }
+    }
+}
