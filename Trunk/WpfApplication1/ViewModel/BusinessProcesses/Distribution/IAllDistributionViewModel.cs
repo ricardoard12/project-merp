7b5747fb77e87bb236825890f5faf10e8f3f2606
k3v1n.ch3n21@gmail.com <k3v1n.ch3n21@gmail.com@04e48588-5cae-8dbb-50e2-7f047fe7dc0a>
2012-03-30 06:38:59 +0000

    implemented AllDistributionViewModel

---
+++ Trunk/WpfApplication1/ViewModel/BusinessProcesses/Distribution/IAllDistributionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Views.BusinessProcesses.Sales.Offer;
+using Views.Stammdaten.Product;
+
+namespace WpfApplication1.ViewModel.BusinessProcesses.Distribution
+{
+    public interface IAllDistributionViewModel
+    {
+        ObservableCollection<ISalesHeaderView> Distribution { get; }
+    }
+}
