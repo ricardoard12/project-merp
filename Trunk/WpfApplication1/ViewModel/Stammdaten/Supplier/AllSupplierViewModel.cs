8b34ecf89bd5eb7f952cf0a9cf6bcff5aae6b5da
k3v1n.ch3n21@gmail.com <k3v1n.ch3n21@gmail.com@04e48588-5cae-8dbb-50e2-7f047fe7dc0a>
2012-03-30 07:07:58 +0000

    implemented AllSupplierViewModel

---
+++ Trunk/WpfApplication1/ViewModel/Stammdaten/Supplier/AllSupplierViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using FrontEnd.DataAccess.Stammdaten.Product;
+using FrontEnd.ViewModel;
+using FrontEnd.ViewModel.Stammdaten.Product;
+using Views.Stammdaten.Product;
+using Views.Stammdaten.Supplier;
+using WpfApplication1.DataAccess.Stammdaten.Supplier;
+
+namespace WpfApplication1.ViewModel.Stammdaten.Supplier
+{
+    public class AllSupplierViewModel: WorkspaceViewModel, IAllSupplierViewModel
+    {
+        private SupplierRepository supplierRepository;
+        private SupplierViewModel supplierViewModel;
+        private ObservableCollection<ISupplierView> suppliers;
+
+        public AllSupplierViewModel()
+        {
+            supplierRepository = new SupplierRepository();
+            supplierViewModel = new SupplierViewModel();
+        }
+
+        public ObservableCollection<ISupplierView> Suppliers
+        {
+            get
+            {
+                if (suppliers == null || suppliers.Count == 0)
+                {
+                    suppliers = new ObservableCollection<ISupplierView>(supplierRepository.GetAllSuppliers());
+                }
+                return suppliers;
+            }
+        }
+    }
+}
---
+++ Trunk/WpfApplication1/ViewModel/Stammdaten/Supplier/IAllSupplierViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Views.Stammdaten.Product;
+using Views.Stammdaten.Supplier;
+
+namespace WpfApplication1.ViewModel.Stammdaten.Supplier
+{
+    public interface IAllSupplierViewModel
+    {
+        ObservableCollection<ISupplierView> Suppliers { get; }
+    }
+}
